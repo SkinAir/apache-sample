7a6ebadac48864fc170fe53422fc7a8e44f3fbb8
SkinAir <71880910+SkinAir@users.noreply.github.com>
2022-04-24 10:39:48 +0800

    edit web-inf

--- apache-example.py
+++ apache-example.py
@@ -19,16 +19,32 @@ _/ __ \\  \/  /\__  \  /     \\____ \|  | _/ __ \
 def examples():
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"}
     payload = '/examples/'
+    payload2 = '/docs/appdev/sample/web/WEB-INF/web.xml'
+    payload3 = '/docs/'
     poc = urls + payload
+    poc2 = urls + payload2
+    poc3 = urls + payload3
     try:
         requests.packages.urllib3.disable_warnings()
         response = requests.get(poc, headers=headers, timeout=15,verify=False)
+        response2 = requests.get(poc2, headers=headers, timeout=7,verify=False)
+        response3 = requests.get(poc3, headers=headers, timeout=7,verify=False)
         if response.status_code == 200 and b"Apache Tomcat Examples" in response.content:
         #if response.status_code == 200:
             print(u'\033[1;31;40m[+]{} is apache Sample directory exists'.format(urls))
             print(response.content)
             with open('./result.txt','a') as f:
-                f.write('[+]' + urls)
+                f.write('[+]' + urls + 'example exists')
+                f.write('\n')
+        elif response2.status_code == 200 and b"Hello, World Application" in response2.content:
+            print(u'\033[1;31;40m[+]{} is apache WEB-INF'.format(urls))
+            with open('./result.txt','a') as f:
+                f.write('[+]' + urls + 'WEB-INF exists')
+                f.write('\n')
+        elif response3.status_code == 200 and b"Apache Tomcat" in response3.content:
+            print(u'\033[1;31;40m[+]{} is apache docs exists'.format(urls))
+            with open('./result.txt','a') as f:
+                f.write('[+]' + urls + 'docs exists')
                 f.write('\n')
         else:
             print('\033[1;32;40m[-]{} None'.format(urls))
